d64d9c513af2fb2dc1cf0c29e23135cc416fe4c6
Aditya Raj <38121656+AdityaRaj2025@users.noreply.github.com>
2022-09-16 10:21:19 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,23 +8,26 @@ public class Program
 		DateTime endDateTime = DateTime.Now.AddDays(10);  
 		TimeSpan difference = endDateTime - startDateTime;  
 		Console.WriteLine("Difference from \n {0} \n and \n {1} \n = \n {2}",endDateTime, startDateTime, difference);  
-		Console.WriteLine("Days = " + difference.Days);    
-		Console.WriteLine("Total Days = " + difference.TotalDays);  
-		Console.WriteLine("Hours = " + difference.Hours);  
+		//Returns the day interval.  
+		Console.WriteLine("Days = " + difference.Days);             
+		//Returns total interval in days.  
+		Console.WriteLine("Total Days = " + difference.TotalDays);             
+		//Returns hour interval leaving days.  
+		Console.WriteLine("Hours = " + difference.Hours);             
+		//Returns total interval in hours.  
 		Console.WriteLine("Total Hours = " + difference.TotalHours);  
-		Console.WriteLine("Minutes = " + difference.Minutes);  
+		//Returns the minute interval leaving hours and days.  
+		Console.WriteLine("Minutes = " + difference.Minutes);             
 		//Returns total interval in minutes.  
-		Console.WriteLine("Total Minutes = " + difference.TotalMinutes);    
+		Console.WriteLine("Total Minutes = " + difference.TotalMinutes); 
 		//Returns the second interval leaving others.  
 		Console.WriteLine("Seconds = " + difference.Seconds);  
 		//Returns total interval in seconds.  
 		Console.WriteLine("Total Seconds =" + difference.TotalSeconds); 
 		//Returns the millisecond interval leaving others.  
 		Console.WriteLine("Milliseconds = " + difference.Milliseconds); 
-		
 		//Returns total interval in milliseconds.  
 		Console.WriteLine("Total Milliseconds =" + difference.TotalMilliseconds);   
-   
 		//Returns total number of ticks.  
 		//Ticks is basically Milliseconds * 10,000  
 		Console.WriteLine("Ticks = " + difference.Ticks); 
